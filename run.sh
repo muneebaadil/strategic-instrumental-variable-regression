34255f4a6114d0e7e3364e957c8fca5d2eeba042
Muneeb Aadil <maadil@mpi-inf.mpg.de>
2023-04-28 17:00:59 +0200

    small typo

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ ROUNDS=(1 10 100 500 1000)
 for round in "${ROUNDS[@]}"; do
     # for round 1, this is the origianl tsls settings.
     python college_admissions_experiments.py --num-applicants 10000 --num-repeat 10 \
-    --admit-all --applicants-per-round ${round} --experiment-name "tsls-${round}-admitall" \
+    --admit-all --applicants-per-round ${round} --experiment-name "tsls-round${round}-admitall" \
     --experiment-root experiments/motivation
 
     python college_admissions_experiments.py --num-applicants 10000 --num-repeat 10 \
