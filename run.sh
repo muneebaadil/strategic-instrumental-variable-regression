fbeaae029499b387fce6e19c92bcdf0241af38c9
Muneeb Aadil <maadil@mpi-inf.mpg.de>
2023-05-02 09:29:59 +0200

    updated bash script

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 # motivation experiments
 ROUNDS=(2 10 100 500 1000)
 for round in "${ROUNDS[@]}"; do
+    # selection
+    python college_admissions_experiments.py --num-applicants 10000 --num-repeat 10 \
+    --applicants-per-round ${round} --experiment-name "tsls-round${round}-admitsome" \
+    --experiment-root experiments/motivation-new --generate 2
+
+    # no selection
     # for round 1, this is the origianl tsls settings.
     python college_admissions_experiments.py --num-applicants 10000 --num-repeat 10 \
     --admit-all --applicants-per-round ${round} --experiment-name "tsls-round${round}-admitall" \
     --experiment-root experiments/motivation-new --generate 2
 
-    python college_admissions_experiments.py --num-applicants 10000 --num-repeat 10 \
-    --applicants-per-round $round --experiment-name "tsls-round${round}-admitsome" \
-    --experiment-root experiments/motivation-new --generate 2
 done
