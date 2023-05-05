910aa5aa426d83735ab6940549e2b7db45483b60
Muneeb Aadil <maadil@mpi-inf.mpg.de>
2023-05-05 14:16:11 +0200

    rearrange

--- college_admissions_experiments.py
+++ college_admissions_experiments.py
@@ -57,61 +57,6 @@ def sample_effort_conversion(EW, n_samples, adv_idx, fixed_effort_conversion):
         EWi[i] -= noise
   return EWi
 
-def generate_data2(n_seen_applicants, admit_all, applicants_per_round, fixed_effort_conversion):
-
-  mean_sat, mean_gpa = 900, 2
-  sigma_sat, sigma_gpa = 200, 0.5
-  EW = np.array([[10.0,0],[0,1.0]])
-  total_applicants, accepted_applicants = 0, 0
-  b, x, y, theta, w, y_hat, adv_idx, disadv_idx = \
-    [], [], [], [], [], [], [], []
-  while accepted_applicants < n_seen_applicants:
-
-    # generate applicants b_t
-    br, gr, adv_idxr, disadv_idxr = generate_bt(
-      applicants_per_round, mean_sat, mean_gpa, sigma_sat, sigma_gpa
-      )
-    
-    # effort conversion
-    EWir = sample_effort_conversion(EW, applicants_per_round, adv_idxr, fixed_effort_conversion)
-
-    # theta
-    thetar = np.random.multivariate_normal([1,1],[[1, 0], [0, 1]], 1)
-    thetar = np.repeat(thetar, repeats=applicants_per_round, axis=0)
-    
-    # x_t
-    xr = compute_xt(EWir, br, thetar)
-    yr = np.clip(np.matmul(xr,theta_star) + gr,0,4) # clipped outcomes
-
-    assert xr.shape == thetar.shape
-    xr_ = xr.copy()
-    xr_[:,0] = (xr_[:,0] - 400) / 300
-    y_hatr = (xr_ * thetar).sum(axis=-1)
-
-    # selection
-    if not admit_all:
-      wr = get_selection(y_hatr)
-    else:
-      wr = np.ones((applicants_per_round,))
-
-    # update data for iteration
-    b.append(br); x.append(xr); y.append(yr); theta.append(thetar)
-    w.append(wr); y_hat.append(y_hatr)
-    adv_idx.append(adv_idxr + total_applicants) # offset 
-    disadv_idx.append(disadv_idxr + total_applicants) # offset
-
-    total_applicants += applicants_per_round
-    accepted_applicants += np.sum(wr)
-
-  b, x, y, theta, w, y_hat, adv_idx, disadv_idx = map(
-      lambda x: np.concatenate(x, axis=0),
-      (b, x, y, theta, w, y_hat, adv_idx, disadv_idx)
-      )
-  return b, x, y, EW, theta, theta_star, w, y_hat, adv_idx, disadv_idx
-
-    
-
-
 def generate_bt(n_samples, mean_sat, mean_gpa, sigma_sat, sigma_gpa):
   assert n_samples % 2 == 0, f"{n_samples} not divisible by 2"
   half = int(n_samples / 2)
@@ -155,6 +100,17 @@ def compute_xt(EWi, b, theta):
   x[:,0] = np.clip(x[:,0],400,1600) # clip to 400 to 1600
   x[:,1] = np.clip(x[:,1],0,4) # clip to 0 to 4.0
   return x
+
+def get_selection(y_hat):
+  assert y_hat.ndim == 1
+  n_applicants = y_hat.shape[0]
+  w = np.zeros_like(y_hat)
+  for i, _y_hat in enumerate(y_hat):
+    y_hat_peers = y_hat[np.arange(n_applicants) != i] # scores of everyone but the current applicant
+    prob = np.mean(y_hat_peers <= _y_hat)
+    w[i] = np.random.binomial(n=1, p=prob)
+  return w
+
 
 def generate_data(num_applicants, admit_all, applicants_per_round, fixed_effort_conversion):
   half = int(num_applicants/2) 
@@ -227,17 +183,58 @@ def generate_data(num_applicants, admit_all, applicants_per_round, fixed_effort_
 
   return b,x,y,EW,theta,theta_star, w, y_hat, adv_idx, disadv_idx
 
-# %%
-def get_selection(y_hat):
-  assert y_hat.ndim == 1
-  n_applicants = y_hat.shape[0]
-  w = np.zeros_like(y_hat)
-  for i, _y_hat in enumerate(y_hat):
-    y_hat_peers = y_hat[np.arange(n_applicants) != i] # scores of everyone but the current applicant
-    prob = np.mean(y_hat_peers <= _y_hat)
-    w[i] = np.random.binomial(n=1, p=prob)
-  return w
+def generate_data2(n_seen_applicants, admit_all, applicants_per_round, fixed_effort_conversion):
 
+  mean_sat, mean_gpa = 900, 2
+  sigma_sat, sigma_gpa = 200, 0.5
+  EW = np.array([[10.0,0],[0,1.0]])
+  total_applicants, accepted_applicants = 0, 0
+  b, x, y, theta, w, y_hat, adv_idx, disadv_idx = \
+    [], [], [], [], [], [], [], []
+  while accepted_applicants < n_seen_applicants:
+
+    # generate applicants b_t
+    br, gr, adv_idxr, disadv_idxr = generate_bt(
+      applicants_per_round, mean_sat, mean_gpa, sigma_sat, sigma_gpa
+      )
+    
+    # effort conversion
+    EWir = sample_effort_conversion(EW, applicants_per_round, adv_idxr, fixed_effort_conversion)
+
+    # theta
+    thetar = np.random.multivariate_normal([1,1],[[1, 0], [0, 1]], 1)
+    thetar = np.repeat(thetar, repeats=applicants_per_round, axis=0)
+    
+    # x_t
+    xr = compute_xt(EWir, br, thetar)
+    yr = np.clip(np.matmul(xr,theta_star) + gr,0,4) # clipped outcomes
+
+    assert xr.shape == thetar.shape
+    xr_ = xr.copy()
+    xr_[:,0] = (xr_[:,0] - 400) / 300
+    y_hatr = (xr_ * thetar).sum(axis=-1)
+
+    # selection
+    if not admit_all:
+      wr = get_selection(y_hatr)
+    else:
+      wr = np.ones((applicants_per_round,))
+
+    # update data for iteration
+    b.append(br); x.append(xr); y.append(yr); theta.append(thetar)
+    w.append(wr); y_hat.append(y_hatr)
+    adv_idx.append(adv_idxr + total_applicants) # offset 
+    disadv_idx.append(disadv_idxr + total_applicants) # offset
+
+    total_applicants += applicants_per_round
+    accepted_applicants += np.sum(wr)
+
+  b, x, y, theta, w, y_hat, adv_idx, disadv_idx = map(
+      lambda x: np.concatenate(x, axis=0),
+      (b, x, y, theta, w, y_hat, adv_idx, disadv_idx)
+      )
+  return b, x, y, EW, theta, theta_star, w, y_hat, adv_idx, disadv_idx
+# %%
 def ols(x,y):
   model = LinearRegression(fit_intercept=True)
   model.fit(x, y)
@@ -261,50 +258,6 @@ def tsls(x,y,theta): # runs until round T
     np.linalg.inv(omega_hat_), lambda_hat_
   )
   return theta_hat_tsls_
-
-def test_params2(num_applicants, x, y, w, theta, theta_star, applicants_per_round):
-  [x_, y_, theta_, w_] = [x.copy(),y.copy(),theta.copy(), w.copy()]
-
-  # admitted datapoints
-  x_, y_, theta_ = x_[w_==1], y_[w_==1], theta_[w_ == 1]
-
-  x_ = x_[:num_applicants]
-  y_ = y_[:num_applicants]
-  theta_ = theta_[:num_applicants]
-
-  assert x_.shape[0] == num_applicants
-  assert y_.shape[0] == num_applicants
-  assert theta_.shape[0] == num_applicants
-  
-  upp_limits = range(applicants_per_round*2,num_applicants+1,2)
-  estimates_list = np.zeros([len(upp_limits),2,2])
-  error_list = np.zeros([len(upp_limits),2])
-
-  i=0
-  for t in tqdm(upp_limits, leave=False):
-    # filtering out rejected students
-    x_round = x_[:t]
-    y_round = y_[:t]
-    theta_round = theta_[:t]
-    # w_round = w_[:t]
-
-    # x_round = x_round[w_round==1]
-    # y_round = y_round[w_round==1]
-    # theta_round = theta_round[w_round==1] # TOASK:limit access to theta as well? 
-
-    # estimates
-    ols_estimate = ols(x_round, y_round) # ols w/ intercept estimate
-    tsls_estimate = tsls(x_round, y_round, theta_round) # 2sls w/ intercept estimate
-    estimates_list[i,:] += [ols_estimate,tsls_estimate]
-
-    # errors
-    ols_error = np.linalg.norm(theta_star-ols_estimate)
-    tsls_error = np.linalg.norm(theta_star-tsls_estimate)
-    error_list[i] = [ols_error,tsls_error]
-    
-    i+=1
-
-  return [estimates_list, error_list]
   
 #%%
 def our(x, y, theta, w):
@@ -358,8 +311,8 @@ def test_params(num_applicants, x, y, w, theta, theta_star, applicants_per_round
 
   # save estimates and errors for every even round 
   upp_limits = range(applicants_per_round*2, num_applicants+1, 2)
-  estimates_list = np.zeros([len(upp_limits),2,2])
-  error_list = np.zeros([len(upp_limits),2])
+  estimates_list = np.zeros([len(upp_limits),2,3])
+  error_list = np.zeros([len(upp_limits),3])
 
   i=0
   for t in tqdm(upp_limits, leave=False):
@@ -377,15 +330,60 @@ def test_params(num_applicants, x, y, w, theta, theta_star, applicants_per_round
     ols_estimate = ols(x_round_admitted, y_round_admitted) # ols w/ intercept estimate
     tsls_estimate = tsls(x_round_admitted, y_round_admitted, theta_round_admitted) # 2sls w/ intercept estimate
     our_estimate = our(x_round, y_round_admitted, theta_round, w_round)
-    # TODO: how do nans operate? 
+    estimates_list[i,:] += [ols_estimate,tsls_estimate,our_estimate]
+
+    # errors
+    ols_error = np.linalg.norm(theta_star-ols_estimate)
+    tsls_error = np.linalg.norm(theta_star-tsls_estimate)
+    our_error = np.linalg.norm(theta_star-our_estimate)
+    error_list[i] = [ols_error,tsls_error,our_error]
+    i+=1
+  return [estimates_list, error_list]
+
+def test_params2(num_applicants, x, y, w, theta, theta_star, applicants_per_round):
+  [x_, y_, theta_, w_] = [x.copy(),y.copy(),theta.copy(), w.copy()]
+
+  # admitted datapoints
+  x_, y_, theta_ = x_[w_==1], y_[w_==1], theta_[w_ == 1]
+
+  x_ = x_[:num_applicants]
+  y_ = y_[:num_applicants]
+  theta_ = theta_[:num_applicants]
+
+  assert x_.shape[0] == num_applicants
+  assert y_.shape[0] == num_applicants
+  assert theta_.shape[0] == num_applicants
+  
+  upp_limits = range(applicants_per_round*2,num_applicants+1,2)
+  estimates_list = np.zeros([len(upp_limits),2,2])
+  error_list = np.zeros([len(upp_limits),2])
+
+  i=0
+  for t in tqdm(upp_limits, leave=False):
+    # filtering out rejected students
+    x_round = x_[:t]
+    y_round = y_[:t]
+    theta_round = theta_[:t]
+    # w_round = w_[:t]
+
+    # x_round = x_round[w_round==1]
+    # y_round = y_round[w_round==1]
+    # theta_round = theta_round[w_round==1] # TOASK:limit access to theta as well? 
+
+    # estimates
+    ols_estimate = ols(x_round, y_round) # ols w/ intercept estimate
+    tsls_estimate = tsls(x_round, y_round, theta_round) # 2sls w/ intercept estimate
     estimates_list[i,:] += [ols_estimate,tsls_estimate]
 
     # errors
     ols_error = np.linalg.norm(theta_star-ols_estimate)
     tsls_error = np.linalg.norm(theta_star-tsls_estimate)
     error_list[i] = [ols_error,tsls_error]
+    
     i+=1
+
   return [estimates_list, error_list]
+
 
 def plot_data(data, condition, name='dataset.pdf'):
   fig,ax=plt.subplots()
@@ -396,80 +394,6 @@ def plot_data(data, condition, name='dataset.pdf'):
   plt.savefig(os.path.join(dirname, name))
   plt.close()
 
-# %% 
-import pickle as pkl
-import time
-experiment_name = time.strftime('%Y%m%d-%H%H%S') if args.experiment_name is None else args.experiment_name
-
-if not args.test_run:
-  dirname = os.path.join(args.experiment_root, f'{experiment_name}')
-else:
-  dirname = os.path.join(args.experiment_root, f'test-run')
-  if os.path.exists(dirname):
-    import shutil
-    shutil.rmtree(dirname)
-  
-os.makedirs(dirname)
-git_hash = get_git_revision_hash()
-with open(os.path.join(dirname, 'git_hash.txt'), 'w+') as f:
-  f.write(git_hash)
-  f.write('\n')
-  f.write(' '.join(sys.argv))
-  
-# %%
-# fix T variable.  
-epochs = args.num_repeat
-half = args.num_applicants
-
-estimates_list_mean = np.zeros((epochs,half,2,2))
-error_list_mean = np.zeros((epochs,half,2))
-estimates_list_mean = []
-error_list_mean = []
-
-#%%
-for i in tqdm(range(epochs)):
-  np.random.seed(i)
-  if args.generate == 1:
-    z,x,y,EW, theta, theta_star, w, y_hat, adv_idx, disadv_idx = generate_data(
-      num_applicants=args.num_applicants, admit_all=args.admit_all, applicants_per_round=args.applicants_per_round,
-      fixed_effort_conversion=args.fixed_effort_conversion
-      )
-  elif args.generate == 2:
-    z,x,y,EW, theta, theta_star, w, y_hat, adv_idx, disadv_idx = generate_data2(
-      n_seen_applicants=args.num_applicants, admit_all=args.admit_all, applicants_per_round=args.applicants_per_round,
-      fixed_effort_conversion=args.fixed_effort_conversion
-    )
-  
-  # plot data.
-  plot_data(y, w, 'dataset_y.png')
-  plot_data(y_hat, w, 'dataset_y_hat.png')
-  try:
-    if args.generate == 1:
-      [estimates_list, error_list] = test_params(args.num_applicants, x, y, w, theta, theta_star, args.applicants_per_round)
-    else:
-      [estimates_list, error_list] = test_params2(args.num_applicants, x, y, w, theta, theta_star, args.applicants_per_round)
-    estimates_list_mean.append(estimates_list[np.newaxis])
-    error_list_mean.append(error_list[np.newaxis])
-  except np.linalg.LinAlgError:
-    pass # record nothing in case the algorithm fails.  
-
-# %%
-estimates_list_mean = np.concatenate(estimates_list_mean,axis=0)
-error_list_mean = np.concatenate(error_list_mean,axis=0)
-
-T = x.shape[0]
-
-filename = os.path.join(dirname, "data")
-# filename = 'college_admission_'+timestr
-with open(filename, 'wb') as f:
-    save = {
-      'estimates_list_mean': estimates_list_mean, 
-      'error_list_mean': error_list_mean,
-      'y': y, 'x': x, 'z': z,  'EW':EW, 'theta':theta, 'theta_star': theta_star, 'w': w, 'y_hat':y_hat
-    }
-    pkl.dump(save, f)
-
-# %%
 def plot_features():
   ## plot first-gen & legacy shift unobservable features (z) to observable (x) 
   fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2,figsize=(12,10)) #constrained_layout=False
@@ -557,11 +481,6 @@ def plot_features():
   fname = os.path.join(dirname, 'fg-ls_shifted_features.png')
   plt.savefig(fname, dpi=500)
   plt.close()
-
-# plt.show()
-
-# %%
-# vars for pyplot
 def plot_error_estimate():
   fig,ax=plt.subplots()
   # TODO: check this. 
@@ -576,6 +495,8 @@ def plot_error_estimate():
               color='darkorange', ecolor='wheat', label='OLS',elinewidth=10)
   plt.errorbar(x, np.mean(error_list_mean,axis=0)[:,1], yerr=np.std(error_list_mean,axis=0)[:,1], 
               color='darkblue', ecolor='lightblue', label='2SLS',elinewidth=10)
+  plt.errorbar(x, np.mean(error_list_mean,axis=0)[:,2], yerr=np.std(error_list_mean,axis=0)[:,2], 
+              color='green', ecolor='lightgreen', label='Our',elinewidth=10)
   plt.ylim(0,.25)
   plt.xticks(fontsize=14)
   plt.yticks(fontsize=14)
@@ -597,8 +518,6 @@ def plot_error_estimate():
   plt.close()
   # plt.show()
 
-
-# %%
 def plot_outcome():
   fig,ax=plt.subplots()
   plt.hist(y,bins='auto',label='combined')
@@ -627,6 +546,85 @@ def plot_outcome():
   plt.close()
   # plt.show()
 
+# %% 
+import pickle as pkl
+import time
+experiment_name = time.strftime('%Y%m%d-%H%H%S') if args.experiment_name is None else args.experiment_name
+
+if not args.test_run:
+  dirname = os.path.join(args.experiment_root, f'{experiment_name}')
+else:
+  dirname = os.path.join(args.experiment_root, f'test-run')
+  if os.path.exists(dirname):
+    import shutil
+    shutil.rmtree(dirname)
+  
+os.makedirs(dirname)
+git_hash = get_git_revision_hash()
+with open(os.path.join(dirname, 'git_hash.txt'), 'w+') as f:
+  f.write(git_hash)
+  f.write('\n')
+  f.write(' '.join(sys.argv))
+  
+# %%
+# fix T variable.  
+epochs = args.num_repeat
+half = args.num_applicants
+
+estimates_list_mean = np.zeros((epochs,half,2,2))
+error_list_mean = np.zeros((epochs,half,2))
+estimates_list_mean = []
+error_list_mean = []
+
+#%%
+for i in tqdm(range(epochs)):
+  np.random.seed(i)
+  if args.generate == 1:
+    z,x,y,EW, theta, theta_star, w, y_hat, adv_idx, disadv_idx = generate_data(
+      num_applicants=args.num_applicants, admit_all=args.admit_all, applicants_per_round=args.applicants_per_round,
+      fixed_effort_conversion=args.fixed_effort_conversion
+      )
+  elif args.generate == 2:
+    z,x,y,EW, theta, theta_star, w, y_hat, adv_idx, disadv_idx = generate_data2(
+      n_seen_applicants=args.num_applicants, admit_all=args.admit_all, applicants_per_round=args.applicants_per_round,
+      fixed_effort_conversion=args.fixed_effort_conversion
+    )
+  
+  # plot data.
+  plot_data(y, w, 'dataset_y.png')
+  plot_data(y_hat, w, 'dataset_y_hat.png')
+  try:
+    if args.generate == 1:
+      [estimates_list, error_list] = test_params(args.num_applicants, x, y, w, theta, theta_star, args.applicants_per_round)
+    else:
+      [estimates_list, error_list] = test_params2(args.num_applicants, x, y, w, theta, theta_star, args.applicants_per_round)
+    estimates_list_mean.append(estimates_list[np.newaxis])
+    error_list_mean.append(error_list[np.newaxis])
+  except np.linalg.LinAlgError:
+    pass # record nothing in case the algorithm fails.  
+
+# %%
+estimates_list_mean = np.concatenate(estimates_list_mean,axis=0)
+error_list_mean = np.concatenate(error_list_mean,axis=0)
+
+T = x.shape[0]
+
+filename = os.path.join(dirname, "data")
+# filename = 'college_admission_'+timestr
+with open(filename, 'wb') as f:
+    save = {
+      'estimates_list_mean': estimates_list_mean, 
+      'error_list_mean': error_list_mean,
+      'y': y, 'x': x, 'z': z,  'EW':EW, 'theta':theta, 'theta_star': theta_star, 'w': w, 'y_hat':y_hat
+    }
+    pkl.dump(save, f)
+
+# %%
+
+# %%
+# vars for pyplot
+
+# %%
 plot_features()
 plot_error_estimate()
 plot_outcome()
