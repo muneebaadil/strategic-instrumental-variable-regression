90b754d96f9bd8f43936f233dff6d016edf788ef
Muneeb Aadil <maadil@mpi-inf.mpg.de>
2023-05-10 17:29:15 +0200

    minor typo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # harris et. al settings. (clipping yes, selection no, random conversion matrix)
-python college_admissions_experiments.py --n-cores 1 --num-repeat 10 --num-applicants 10000 --admit-all --applicants-per-round 1 --clip --experiment-root experiments --experiment-name clipped-yes-selection-yes-random-effort-matrix-d1-algo --stream
+python college_admissions_experiments.py --n-cores 1 --num-repeat 10 --num-applicants 10000 --admit-all --applicants-per-round 1 --clip --experiment-root experiments --experiment-name clipped-yes-selection-no-random-effort-matrix-d1-algo --stream
 
 # harris et. al + selection (clipping yes, selection yes, random conversion matrix) 
 # need round parameters. 
